5e97b9cea95deb5c3687a71bc95eedf8c6d0951e
Scott Westover <scottwestover2006@gmail.com>
2018-11-23 02:12:21 -0500

    finished part two of tutorial

--- server/authoritative_server/js/game.js
+++ server/authoritative_server/js/game.js
@@ -20,16 +20,14 @@ const config = {
   autoFocus: false
 };
 
-function preload() {}
+function preload() {
+  this.load.image('ship', 'assets/spaceShips_001.png');
+}
 
 function create() {
-  io.on('connection', function (socket) {
-    console.log('a user connected');
-    socket.on('disconnect', function () {
-      console.log('user disconnected');
-    });
-  });
-  /*
+  const self = this;
+  this.players = this.physics.add.group();
+
   io.on('connection', function (socket) {
     console.log('a user connected');
     // create a new player and add it to our players object
@@ -40,24 +38,43 @@ function create() {
       playerId: socket.id,
       team: (Math.floor(Math.random() * 2) == 0) ? 'red' : 'blue'
     };
+    // add player to server
+    addPlayer(self, players[socket.id]);
     // send the players object to the new player
     socket.emit('currentPlayers', players);
     // update all other players of the new player
     socket.broadcast.emit('newPlayer', players[socket.id]);
 
-    // when a player disconnects, remove them from our players object
     socket.on('disconnect', function () {
       console.log('user disconnected');
+      // remove player from server
+      removePlayer(self, socket.id);
       // remove this player from our players object
       delete players[socket.id];
       // emit a message to all players to remove this player
       io.emit('disconnect', socket.id);
     });
   });
-  */
 }
 
 function update() {}
+
+function addPlayer(self, playerInfo) {
+  const player = self.physics.add.image(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  player.setDrag(100);
+  player.setAngularDrag(100);
+  player.setMaxVelocity(200);
+  player.playerId = playerInfo.playerId;
+  self.players.add(player);
+}
+
+function removePlayer(self, playerId) {
+  self.players.getChildren().forEach((player) => {
+    if (playerId === player.playerId) {
+      player.destroy();
+    }
+  });
+}
 
 const game = new Phaser.Game(config);
 window.gameLoaded();
